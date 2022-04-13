7b880bd8a9f38ed83f99f61031ef3ff6f53c7d7e
Akshay Benny <976Ab482a*123#>
2022-04-14 01:50:20 +0530

    basic setup

---
+++ app/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bottom-0 bg-black text-center"><p className="font-light text-white">Made by Akshay Benny</p></div>
+  )
+}
+
+export default Footer
---
+++ app/pages/index.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="h-screen w-full bg-gray-100"><p>Hello world</p></div>;
+}
