5b558b19eeb0f9f5b05721b43625b03c8766b497
Akshay <akshaybennygh@gmail.com>
2022-12-26 19:42:43 +0530

    basic setup

--- app/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div className="bottom-0 bg-black text-center"><p className="font-light text-white">Made by Akshay Benny</p></div>
-  )
-}
-
-export default Footer
--- app/pages/index.js
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="h-screen w-full bg-gray-100"><p>Hello world</p></div>;
-}
