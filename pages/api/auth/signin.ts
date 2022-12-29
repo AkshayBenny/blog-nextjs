8ee41566b271e1bf42edcc818a6646bb628b0f55
Akshay <akshaybennygh@gmail.com>
2022-12-29 22:37:12 +0530

    user signin

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -1,15 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import dbConnect from '../../../lib/dbConnect'
 import generateToken from '../../../lib/generateToken'
-import { hashPassword } from '../../../lib/hashPass'
+import { matchPassword } from '../../../lib/hashPass'
 import User from '../../../models/userModel'
-type Data = {
-	fname: string
-	lname: string
-	phone: string
-	email: string
-	password: string
-}
 
 export default async function handler(
 	req: NextApiRequest,
@@ -18,18 +12,34 @@ export default async function handler(
 	if (req.method !== 'POST') {
 		return res.status(405).json({ message: 'Method not allowed' })
 	}
+	await dbConnect()
+	const { email, password } = req.body
 
-	const { fname, lname, phone, email, password } = req.body
-	const hashedPass = await hashPassword(password)
-	await User.create({
-		fname,
-		lname,
-		phone,
-		email,
-		password: hashedPass,
-	})
+	if (!email || !password) {
+		res.status(401).send('Incorrect username or password')
+	}
+
+	let user
+	try {
+		user = await User.findOne({
+			email: email.toString(),
+		})
+	} catch (error) {
+		console.log(error)
+	}
+
+	if (!user) {
+		return res.status(404).json({ message: 'User not found.' })
+	}
+
+	const hashedPass = user.password
+	const correctPass = await matchPassword(password, hashedPass)
+
+	if (!correctPass) {
+		return res.status(404).json({ message: 'Incorrect email or password.' })
+	}
 
 	const token = await generateToken()
 
-	res.status(201).json({ message: 'User registered', token })
+	res.status(201).json({ message: 'Signin successfull', token, user })
 }
--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,43 +1,52 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
 const Signin = () => {
 	const [userData, setUserData] = useState({
-		fname: '',
-		lname: '',
-		phone: '',
 		email: '',
 		password: '',
 	})
 
-	return (
-		<div>
-			<form>
-				<input
-					type='text'
-					name='fname'
-				/>
-				<input
-					type='text'
-					name='lname'
-				/>
-				<input
-					type='number'
-					name='phone'
-				/>
+	const signinHandler = async (e: any) => {
+		e.preventDefault()
+		try {
+			const { data } = await axios.post('/api/auth/signin', userData, {
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			localStorage.setItem('userInfo', JSON.stringify(data.user))
+			localStorage.setItem('token', data.token)
+		} catch (error) {
+			console.log(error)
+			window.alert('Something went wrong.')
+		}
+	}
 
+	return (
+		<div className='flex items-center justify-center w-screen h-screen'>
+			<form
+				onSubmit={signinHandler}
+				className='border p-5 flex flex-col items-center justify-center'>
 				<input
 					type='email'
-					name='email address'
+					onChange={(e) =>
+						setUserData((prev) => ({
+							...prev,
+							email: e.target.value,
+						}))
+					}
 				/>
 				<input
 					type='password'
-					name='password'
+					onChange={(e) =>
+						setUserData((prev) => ({
+							...prev,
+							password: e.target.value,
+						}))
+					}
 				/>
-				<input
-					type='password'
-					name='confirm password'
-				/>
-        <button>Signin</button>
+				<button>Signin</button>
 			</form>
 		</div>
 	)
